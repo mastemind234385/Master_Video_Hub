6b16dd1577314ef4d2b4385ebd12b45daf4909a0
mastemind234385 <mastermind191209@gmail.com>
2026-08-12 23:30:43 +0600

    Add files via upload

---
+++ web.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Video Downloader Bot is running!"
+
+
+@app.route("/health")
+def health():
+    return "OK"
+
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
