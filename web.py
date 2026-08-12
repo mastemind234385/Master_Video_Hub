0328420c6f63feec323a5a867be649f04f8607e5
mastemind234385 <mastermind191209@gmail.com>
2026-08-12 23:49:19 +0600

    Add run_web function to handle dynamic port
    Refactor web.py to add run_web function for dynamic port.

--- web.py
+++ web.py
@@ -1,9 +1,6 @@
-import os
-import threading
-
 from flask import Flask
-
-from main import create_bot
+import threading
+import os
 
 
 app = Flask(__name__)
@@ -19,12 +16,10 @@ def health():
     return "OK"
 
 
-def start_bot():
-    bot_app = create_bot()
-    bot_app.run_polling()
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
 
-
-threading.Thread(
-    target=start_bot,
-    daemon=True
-).start()
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
